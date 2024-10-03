5bc99192085d9d9ff94112490a40656491c885f7
Hassan <hassandev@zoorganized.com>
2024-10-03 16:34:43 -0700

    sliderThree

--- index.js
+++ index.js
@@ -2,61 +2,92 @@
 
 let slideIndex = 0;
 
-function showSlide () {
-  let slides = document.getElementsByClassName ('slide');
+function showSlide() {
+  let slides = document.getElementsByClassName("slide");
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
+    slides[i].style.display = "none";
   }
-  slides[slideIndex].style.display = 'block';
+  slides[slideIndex].style.display = "block";
 }
 
-function nextSlide () {
+function nextSlide() {
   slideIndex++;
-  if (slideIndex >= document.getElementsByClassName ('slide').length) {
+  if (slideIndex >= document.getElementsByClassName("slide").length) {
     slideIndex = 0;
   }
-  showSlide ();
+  showSlide();
 }
 
-function prevSlide () {
+function prevSlide() {
   slideIndex--;
   if (slideIndex < 0) {
-    slideIndex = document.getElementsByClassName ('slide').length - 1;
+    slideIndex = document.getElementsByClassName("slide").length - 1;
   }
-  showSlide ();
+  showSlide();
 }
 
-showSlide ();
+showSlide();
 
-setInterval (nextSlide, 2000);
+setInterval(nextSlide, 2000);
 
 // Slider Two
 let slideIndexTwo = 0;
 
-function showSlideTwo () {
-  let slidesTwo = document.getElementsByClassName ('slideTwo');
+function showSlideTwo() {
+  let slidesTwo = document.getElementsByClassName("slideTwo");
   for (let i = 0; i < slidesTwo.length; i++) {
-    slidesTwo[i].style.display = 'none';
+    slidesTwo[i].style.display = "none";
   }
-  slidesTwo[slideIndexTwo].style.display = 'block';
+  slidesTwo[slideIndexTwo].style.display = "block";
 }
 
-function nextSlideTwo () {
+function nextSlideTwo() {
   slideIndexTwo++;
-  if (slideIndexTwo >= document.getElementsByClassName ('slideTwo').length) {
+  if (slideIndexTwo >= document.getElementsByClassName("slideTwo").length) {
     slideIndexTwo = 0;
   }
-  showSlideTwo ();
+  showSlideTwo();
 }
 
-function prevSlideTwo () {
+function prevSlideTwo() {
   slideIndexTwo--;
   if (slideIndexTwo < 0) {
-    slideIndexTwo = document.getElementsByClassName ('slideTwo').length - 1;
+    slideIndexTwo = document.getElementsByClassName("slideTwo").length - 1;
   }
-  showSlideTwo ();
+  showSlideTwo();
 }
 
-showSlideTwo ();
+showSlideTwo();
 
-setInterval (nextSlideTwo, 2000);
+setInterval(nextSlideTwo, 2000);
+
+// Slider Three
+let slideIndexThree = 0;
+
+function showSlideThree() {
+  let slidesThree = document.getElementsByClassName("slideThree");
+  for (let i = 0; i < slidesThree.length; i++) {
+    slidesThree[i].style.display = "none";
+  }
+  slidesThree[slideIndexThree].style.display = "block";
+}
+
+function nextSlideThree() {
+  slideIndexThree++;
+  if (slideIndexThree >= document.getElementsByClassName("slideThree").length) {
+    slideIndexThree = 0;
+  }
+  showSlideThree();
+}
+
+function prevSlideThree() {
+  slideIndexThree--;
+  if (slideIndexThree < 0) {
+    slideIndexThree = document.getElementsByClassName("slideThree").length - 1;
+  }
+  showSlideThree();
+}
+
+showSlideThree();
+
+setInterval(nextSlideThree, 2000);
